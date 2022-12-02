931321db7e68ace39a3f3270d6e7ec866dfb6389
erwan-h-dev <erwan.h.dev@gmail.com>
2022-12-02 15:34:42 +0100

    improve login and register page + make forgot password page #15 #16

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,93 +3,100 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Core\Request;
 use App\Core\Controller;
-use App\Repository\UserRepository;
-
 class SecurityController extends Controller
 {
 
     public function login()
     {
+        $userRepository = $this->entityManager->getRepository(User::class);
+        $request = new Request();
+        $error = [];
+
         if($this->getUser()){
             $this->redirectRoute('home');
         }
 
-        $userRepository = $this->entityManager->getRepository(User::class);
-        if (count($_POST) > 0) {
-
-            $user = $userRepository->findOneBy(['email' => $_POST['login']['email']]);
-
-            if ($user && \password_verify($_POST['login']['password'], $user->getPassword())) {
-
-                $date = new \DateTime();
-                $user->setLastLogin($date->format('Y-m-d H:i:s'));
-                $this->entityManager->update($user);
-                $this->setUser($user->getId());
-                $this->redirectRoute('home');
-            }
+        if (empty($request->request())) {
+            return $this->render('security/login.html.twig', []);
         }
 
-        $location = 'login';
+        $user = $userRepository->findOneBy(['email' => $request->getRequest('email')]);
 
-        return $this->render('security/login.html.twig', [
-            'location' => $location
-        ]);
+        if (!$user ) {
+            $error['email'] = 'Email not found';
+        }
+
+        if(!password_verify($request->getRequest('password'), $user->getPassword())){
+            $error['password'] = 'Password not found';
+        }
+
+        if (!empty($error)) {
+            var_dump($error, $request->request());
+            return $this->render('security/login.html.twig', ['error' => $error]);
+        }
+        
+        $date = new \DateTime();
+        $user->setLastLogin($date->format('Y-m-d H:i:s'));
+        $this->entityManager->update($user);
+        $this->setUser($user->getId());
+        $this->redirectRoute('home');
+
+        
     }
 
     public function register()
     {
         $userRepository = $this->entityManager->getRepository(User::class);
+        $request = new Request();
 
-        if (count($_POST) > 0) {
-            
-            $user = $userRepository->findOneBy(['email' => $_POST['register']['email']]);
+        $error = [];
 
-            if(preg_match("/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/", $_POST['register']['password'])){
-                if($_POST['register']['password'] === $_POST['register']["password_confirm"]){
-                    if (!$user){
-
-                        $user = new User();
-        
-                        $date = new \DateTime();
-            
-                        $user->setUserName($_POST['register']['userName'])
-                            ->setFirstName($_POST['register']['firstName'])
-                            ->setLastName($_POST['register']['lastName'])
-                            ->setEmail($_POST['register']['email'])
-                            ->setPassword(password_hash($_POST['register']['password'], PASSWORD_BCRYPT))
-                            ->setCreatedAt($date->format('Y-m-d H:i:s'))
-                            ->setUpdatedAt($date->format('Y-m-d H:i:s'))
-                            ->setLastLogin($date->format('Y-m-d H:i:s'))
-                            ->setRole('user');
-            
-                        $this->entityManager->insert($user);
-                        
-                        return $this->render('security/login.html.twig', []);
-                    }else{
-                        return $this->render('security/register.html.twig', [
-                            'error' => [
-                                'email' => 'Email already exists'
-                            ]
-                        ]);
-                    }
-                }else{
-                    return $this->render('security/register.html.twig', [
-                        'error' => [
-                            'password' => 'Your passwords do not match'
-                        ]
-                    ]);
-                }
-            }else{
-                return $this->render('security/register.html.twig', [
-                    'error' => [
-                        'password' => 'Your password must be at least 8 characters long or no contain numbers'
-                    ]
-                ]);
-            }
+        if (empty($request->request())) {
+            return $this->render('security/register.html.twig', []);
         }
 
-        return $this->render('security/register.html.twig', []);
+        $user = $userRepository->findOneBy(['email' => $request->getRequest('email')]);
+
+        if ($user) {
+            $error['password'] = 'Email already exists';
+        }
+        
+        if($request->getRequest('password') !== $request->getRequest('password_confirm')){
+            $error['password'] = 'Your passwords do not match';
+        }
+
+        if (!preg_match("/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/", $request->getRequest('password'))) {
+            $error['password'] = 'Your password must be at least 8 characters long or no contain numbers';
+        }
+
+        if(!empty($error)){
+            var_dump($request->getRequest('password'));
+            return $this->render('security/register.html.twig', [
+                'error' => $error,
+                'request' => $request->request()
+            ]);
+        }
+
+        $user = new User();
+
+        $date = new \DateTime();
+
+        $user->setUserName($request->getRequest('userName'))
+            ->setFirstName($request->getRequest('firstName'))
+            ->setLastName($request->getRequest('lastName'))
+            ->setEmail($request->getRequest('email'))
+            ->setPassword(password_hash($request->getRequest('password'), PASSWORD_BCRYPT))
+            ->setCreatedAt($date->format('Y-m-d H:i:s'))
+            ->setUpdatedAt($date->format('Y-m-d H:i:s'))
+            ->setLastLogin($date->format('Y-m-d H:i:s'))
+            ->setRole('user')
+        ;
+
+        $this->entityManager->insert($user);
+        
+        return $this->render('security/login.html.twig', []);
     }
 
     public function logout()
@@ -98,4 +105,48 @@ class SecurityController extends Controller
 
         $this->redirectRoute('login');
     }
+
+    public function forgotPassword()
+    {
+        $request = new Request();
+        $error = [];
+        
+        if(count($request->request()) == 0){
+            return $this->render('security/forgotPassword.html.twig', [
+                'error' => $error
+            ]);
+        }
+
+        $userRepository = $this->entityManager->getRepository(User::class);
+        $user = $userRepository->findOneBy(['email' => $request->getRequest('email')]);
+
+        if (!$user) {
+            $error['email'] = 'Email does not exist';
+        }
+        
+        if ($request->getRequest('new-password') !== $request->getRequest('confirm-password')) {
+            $error['password'] = 'Your passwords do not match';
+        }
+
+        if (!preg_match("/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/", $request->getRequest('new-password'))) {
+            $error['password'] = 'Your passwords must be at least 8 characters long or no contain numbers';
+        }
+
+        
+        if(!empty($error)){
+            var_dump($request->request());
+            return $this->render('security/forgotPassword.html.twig', [
+                'error' => $error
+            ]);
+        }
+
+        $user->setPassword(password_hash($request->getRequest('new-password'), PASSWORD_BCRYPT));
+
+        $this->entityManager->update($user);
+
+       
+        $this->redirectRoute('login');
+    }
+
+    
 }
